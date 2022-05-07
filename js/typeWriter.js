ad409305d5fb99816cbbc894f59c93583fecfbcb
rizkyhaksono <mrizkyhaksono@gmail.com>
2022-05-07 20:03:57 +0700

    first commit

---
+++ js/typeWriter.js
@@ -0,0 +1,33 @@
+let i = 0;
+let text1 = "Hai, Anggun!";
+let text2 = "My life is more meaningful with you in it.";
+let speed = 100;
+
+function typeWriter(text, para) {
+    if (ok == 2) {
+        clearInterval(typeInterval);
+    }
+    if (i < text.length) {
+        document.getElementById(para).innerHTML += text.charAt(i);
+        i++;
+        speed = Math.random() * 50 + 100;
+    } else {
+        if (ok == 0) {
+            i = 0;
+        }
+        ok += 1;
+    }
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+typeInterval = setInterval(function () {
+    if (ok == 0) {
+        typeWriter(text1, "txt1");
+    } else if (ok == 1) {
+        typeWriter(text2, "txt2");
+    }
+}, 100);
+//};
